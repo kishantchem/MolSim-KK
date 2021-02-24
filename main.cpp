ce1726cf8bb71b8f47f6fa359f872ee60f9cb51d
Dr. Kishant Kumar <kishant9528@gmail.com>
2021-02-24 11:37:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,16 @@ int main ()
 //Read the information related to simulation from the users
 // ensemble, mass of the atom, temperature, number of atoms, size of the box, sigma, epsilon, time step, number of time steps, 
 // 
+  
+  Step1. Declare variables, Constants and Functions
+    
+    Step2. 
+          Take Users information
+    Step3. 
+          Build atoms -box 
+    Step4. 
+          
+    
+  
 
 }
